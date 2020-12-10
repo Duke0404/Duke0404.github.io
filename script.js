bcdc8b99f7ec87823cf8ddc5fa121e7885738a92
Duke0404 <devansh0404@gmail.com>
2020-12-10 02:38:27 +0100

    v

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function scrollPanel() {
-    document.getElementsByClassName("panel").style.top -= "100vh";
-}
-
-document.getElementsByClassName("panel").onscroll = scrollPanel();
+function createRipple(event) {
+    const button = event.currentTarget;
+    const circle = document.createElement("span");
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${event.clientX - (button.offsetLeft + radius)}px`;
+    circle.style.top = `${event.clientY - (button.offsetTop + radius)}px`;
+    circle.classList.add("ripple");
+    const ripple = button.getElementsByClassName("ripple")[0];
+    if (ripple) {
+        ripple.remove();
+    }
+    button.appendChild(circle);
+    const buttons = document.getElementsByTagName("button");
+    for (const button of buttons) {
+        button.addEventListener("click", createRipple);
+    }
+  }
